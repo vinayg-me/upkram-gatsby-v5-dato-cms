c4e312e2309e554a2e3f753f3d1c6dd474db61fd
vinayg-me <work@vinayg.me>
2023-07-02 19:08:01 +0530

    Second Footer Col Completed

--- src/components/footer.css.ts
+++ src/components/footer.css.ts
@@ -17,3 +17,59 @@ export const EmbeddedTextStyle = style({
         },
     },
 })
+
+export const HeightFull = style({
+    height: "100%",
+})
+
+export const FullHeightCol = style({
+    height: "100%",
+    width: "33%",
+    "@media": {
+        [media.smallMobile]: {
+            width: "100%",
+        },
+        [media.mobile]: {
+            width: "100%",
+        }
+    },
+})
+
+export const LogoContainer = style({
+    display: "flex",
+    backgroundColor: theme.colors.white,
+    width: "100%",
+    height: "100%",
+    padding: "8px",
+    flexDirection: "column",
+})
+
+export const FooterTitle = style({
+    fontSize: theme.fontSizes[2],
+    fontWeight: 600,
+    marginBottom: 0,
+})
+
+export const FooterText = style({
+    display: "flex",
+    alignItems: "center",
+    fontSize: theme.fontSizes[2],
+})
+
+export const FooterIcons = style({
+    marginRight: "8px",
+})
+
+export const SecondCol = style({
+    alignItems: "flex-start",
+    height: '100%',
+    width: '33%',
+    "@media": {
+        [media.smallMobile]: {
+            width: "100%",
+        },
+        [media.mobile]: {
+            width: "100%",
+        }
+    },
+})
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,10 +2,11 @@ import * as React from "react"
 import { graphql, useStaticQuery } from "gatsby"
 import {
   Twitter,
-  Twitch,
   Instagram,
   Facebook,
   Youtube,
+  MapPin,
+  Mail,
 } from "react-feather"
 import {
   Container,
@@ -116,47 +117,42 @@ export default function Footer() {
 
   return (
     <Box as="footer" paddingY={4} background="dark">
-      <Container style={{
-        height: "100%",
-      }}>
-        <Flex variant="column" style={{
-          height: "100%",
-        }} responsive>
-          <NavLink to="/" style={{
-            height: "100%",
-            width: "33%",
-          }}>
-            <div style={{
-              display: "flex",
-              backgroundColor: theme.colors.white,
-              width: "100%",
-              height: "100%",
-              padding: "8px",
-              flexDirection: "column",
-            }}>
+      <Container className={styles.HeightFull}>
+        <Flex variant="column" className={styles.HeightFull} responsive style={{
+          alignItems: "flex-start"
+        }}>
+          <NavLink to="/" className={styles.FullHeightCol}>
+            <div className={styles.LogoContainer}>
               <VisuallyHidden>Home</VisuallyHidden>
               <BrandLogo />
               <EmbeddedText className={styles.EmbeddedTextStyle} dangerouslySetInnerHTML={{ __html: footerCtaText }}></EmbeddedText>
               <div>{footerCtaButton.length > 0 && <Button variant="red" to={footerCtaButton[0].href}>{footerCtaButton[0].text}</Button>}</div>
             </div>
           </NavLink>
-          <Space />
-          <FlexList>
-            {socialLinks &&
-              socialLinks.map((link) => {
-                const url = getSocialURL(link)
-                return (
-                  url && (
-                    <li key={link.id}>
-                      <IconLink to={url}>
-                        <VisuallyHidden>{getSocialName(link)}</VisuallyHidden>
-                        {getSocialIcon(link)}
-                      </IconLink>
-                    </li>
+          <Space size={1} />
+          <Flex variant="column" className={styles.SecondCol}>
+            <Text variant="body" className={styles.FooterTitle} as="p">GET IN TOUCH</Text>
+            <Text variant="small" className={styles.FooterText} as="p"><MapPin size={24} className={styles.FooterIcons} />{address}</Text>
+            <Text variant="small" as="p" className={styles.FooterText}><Mail size={24} className={styles.FooterIcons} />{emailAddress}</Text>
+            <Text variant="body" className={styles.FooterTitle} as="p">FOLLOW US</Text>
+            <FlexList>
+              {socialLinks &&
+                socialLinks.map((link) => {
+                  const url = getSocialURL(link)
+                  return (
+                    url && (
+                      <li key={link.id}>
+                        <IconLink to={url}>
+                          <VisuallyHidden>{getSocialName(link)}</VisuallyHidden>
+                          {getSocialIcon(link)}
+                        </IconLink>
+                      </li>
+                    )
                   )
-                )
-              })}
-          </FlexList>
+                })}
+            </FlexList>
+          </Flex>
+
         </Flex>
         <Space size={5} />
         {/* <Flex variant="start" responsive>
