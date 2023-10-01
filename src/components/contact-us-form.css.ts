d00fcaa31ab8eab951fa87d969fc7e08e947e49e
vinayg-me <work@vinayg.me>
2023-10-01 22:23:14 +0530

    Need to move from gatsby cloud to netlify

---
+++ src/components/contact-us-form.css.ts
@@ -0,0 +1,76 @@
+import { style } from "@vanilla-extract/css"
+import { theme } from "../theme.css"
+import { media } from "./ui.css"
+
+export const FormLabel = style({
+    fontFamily: theme.fonts.heading,
+    fontSize: theme.fontSizes[2],
+    fontWeight: theme.fontWeights.normal,
+    display: 'block'
+})
+
+export const FormInput = style({
+    height: 48,
+    lineHeight: 24,
+    width: 320,
+    marginTop: 8,
+    marginBottom: 8,
+    border: '1px solid var(--Light-Stroke, #D9D9D9)',
+    "@media": {
+        [media.smallMobile]: {
+            width: '100%',
+        },
+        [media.mobile]: {
+            width: '100%',
+        }
+    }
+})
+
+export const FormTextArea = style({
+    height: 96,
+    lineHeight: 24,
+    width: 480,
+    marginTop: 8,
+    marginBottom: 8,
+    border: '1px solid var(--Light-Stroke, #D9D9D9)',
+    overflow: 'hidden',
+    "@media": {
+        [media.smallMobile]: {
+            width: '100%',
+        },
+        [media.mobile]: {
+            width: '100%',
+        }
+    }
+})
+
+export const RowContainer = style({
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr',
+    gridGap: 10,
+    "@media": {
+        [media.smallMobile]: {
+            gridTemplateColumns: '1fr',
+        },
+        [media.mobile]: {
+            gridTemplateColumns: '1fr',
+        }
+    }
+})
+
+export const ColContainer = style({
+    padding: 8
+})
+
+
+
+// element.style {
+//     height: 48px;
+//     line-height: 24px;
+//     width: 360px;
+//     /* margin: 16px; */
+//     margin-top: 16px;
+//     /* border-color: red; */
+//     border-radius: 4px;
+//     border: 1px solid var(--Light-Stroke, #D9D9D9);
+// }
